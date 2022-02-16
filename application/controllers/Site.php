acf975f38d50914d6bda5035d91808118b79b111
Manishrai31 <saajanyankee@gmail.com>
2022-02-16 18:55:32 +0530

    Add Live Tournament Result function

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -3092,10 +3092,10 @@ public function globalLeaderboard(){
 	/*  
 	echo $weeklyRank;
 	echo "<pre>"; print_r($weeklyRankList); echo "</pre>";
-		echo "<br><br>";
+	echo "<br><br>";
 	echo $monthlyRank;
 	echo "<pre>"; print_r($monthlyRankList); echo "</pre>";
-	  */
+	*/
 	$data['myWeeklyRank'] = $weeklyRank;
 	$data['myWeeklyRankList'] = $weeklyRankList;
 	$data['myMonthlyRank'] = $monthlyRank;
@@ -3104,6 +3104,218 @@ public function globalLeaderboard(){
 	
 }	
 
+//  Tournament Live Result 
 
+public function liveTournamentsResults(){
+		
+	$todayHrsStart =  date('Y-m-d', strtotime('-1 day'));
+	$todayHrsEnd =  date('Y-m-d');
+	
+	
+	//	$list = $this->db->query("SELECT `tbl_tournaments`.tournament_id,`tbl_tournaments`.tournament_section, `tbl_tournaments_fee_rewards`.fee_tournament_prize_1, `tbl_tournaments_fee_rewards`.fee_tournament_prize_2,`tbl_tournaments_fee_rewards`.fee_tournament_prize_3,`tbl_tournaments_fee_rewards`.fee_tournament_prize_4,`tbl_tournaments_fee_rewards`.fee_tournament_prize_5,`tbl_tournaments_fee_rewards`.fee_tournament_prize_6,`tbl_tournaments_fee_rewards`.fee_tournament_prize_7,`tbl_tournaments_fee_rewards`.fee_tournament_prize_8 FROM tbl_tournaments left join tbl_tournaments_fee_rewards on `tbl_tournaments_fee_rewards`.fee_turnament_id = `tbl_tournaments`.tournament_id WHERE  `tbl_tournaments`.tournament_end_date BETWEEN '$todayHrsStart' AND '$todayHrsEnd' ")->result_array();
+	$list = $this->db->query("SELECT `tbl_tournaments`.tournament_id,`tbl_tournaments`.tournament_section,`tbl_tournaments_fee_rewards`.fee_tournament_rewards, `tbl_tournaments_fee_rewards`.fee_tournament_prize_1, `tbl_tournaments_fee_rewards`.fee_tournament_prize_2,`tbl_tournaments_fee_rewards`.fee_tournament_prize_3,`tbl_tournaments_fee_rewards`.fee_tournament_prize_4,`tbl_tournaments_fee_rewards`.fee_tournament_prize_5,`tbl_tournaments_fee_rewards`.fee_tournament_prize_6,`tbl_tournaments_fee_rewards`.fee_tournament_prize_7,`tbl_tournaments_fee_rewards`.fee_tournament_prize_8,`tbl_tournaments_fee_rewards`.fee_tournament_prize_9	 FROM tbl_tournaments left join tbl_tournaments_fee_rewards on `tbl_tournaments_fee_rewards`.fee_turnament_id = `tbl_tournaments`.tournament_id WHERE  `tbl_tournaments`.tournament_end_date = '$todayHrsStart' ")->result_array();
+	
+	//	echo $this->db->last_query();
+	if(is_array($list) && count($list)>0){
+		 
+		foreach($list as $tRow)
+		{
+			
+			$t_id = $tRow['tournament_id'];
+			
+			
+				// Get the tournament result status
+			
+			$tResult = $this->db->query("SELECT count(*) as no_rows FROM tbl_tournaments_results WHERE result_t_id = '$t_id' ")->row_array();
+	
+			 
+			if($tResult['no_rows']<=0){
+				 
+			
+				// Manage a aaray for rank-wise prizes
+				$t_prize_1 = $tRow['fee_tournament_prize_1'];  	// For 1st rank
+				$t_prize_2 = $tRow['fee_tournament_prize_2'];  	// For 2nd rank
+				$t_prize_3 = $tRow['fee_tournament_prize_3'];  	// For 3rd rank
+				$t_prize_4 = $tRow['fee_tournament_prize_4']; 	// For 4-5 rank 
+				$t_prize_5 = $tRow['fee_tournament_prize_5'];  	// For 6-10 rank
+				$t_prize_6 = $tRow['fee_tournament_prize_6'];  	// For 11-25 rank
+				$t_prize_7 = $tRow['fee_tournament_prize_7'];  	// For 26-50 rank
+				$t_prize_8 = $tRow['fee_tournament_prize_8']; 	// For 51-100 rank
+				$t_prize_9 = $tRow['fee_tournament_prize_9'];	// For 101-200 rank
+				$array['prizes'] = array("1"=>$t_prize_1, "2"=>$t_prize_2, "3"=>$t_prize_3);
+				
+				if($t_prize_4)
+				{
+					$array['prizes']["4"] = $t_prize_4;
+					$array['prizes']["5"] = $t_prize_4;
+				}
+				if($t_prize_5)
+				{
+					for($i = 6; $i<=10; $i++)
+					{
+						$array['prizes'][$i] = $t_prize_5;
+					}
+				}
+				if($t_prize_6)
+				{
+					for($i = 11; $i<=25; $i++)
+					{
+						$array['prizes'][$i] = $t_prize_6;
+					}
+				}
+				if($t_prize_7)
+				{
+					for($i = 26; $i<=50; $i++)
+					{
+						$array['prizes'][$i] = $t_prize_7;
+					}
+				}
+				if($t_prize_8)
+				{
+					for($i = 51; $i<=100; $i++)
+					{
+						$array['prizes'][$i] = $t_prize_8;
+					}
+				}
+				if($t_prize_9)
+				{
+					for($i = 101; $i<=200; $i++)
+					{
+						$array['prizes'][$i] = $t_prize_8;
+					}
+				}
+				
+			
+				// Get the tournament players
+				$no_player_selected = count($array['prizes']);
+					
+				$playersList = $this->db->query("SELECT * FROM tbl_tournaments_players WHERE player_t_id = '$t_id' AND player_score > '0' ORDER BY player_score DESC, player_score_updated  ASC LIMIT $no_player_selected")->result_array();
+				//  print_r($playersList);
+				//  die();
+				
+				if(is_array($playersList) && count($playersList)>0){
+					
+					$highest_score = $playersList[0]['player_score'];
+					$rank = 1;
+					$arrIndex = 1;
+					
+					foreach($playersList as $player){
+						if($player['player_score'] >0){
+							$userId = $player['player_user_id'];
+							$userInfo = $this->SITEDBAPI->validateUser($userId);
+							if($list[0]['fee_tournament_rewards']==3)
+							{
+							
+								$data['tg_player_user_id'] = $userId;
+								$data['tg_player_msisdn'] = $userInfo['user_phone'];
+								$data['tg_t_id'] = $player['player_t_id'];
+								$data['tg_player_id'] = $player['player_id'];
+								$data['tg_player_score'] = $player['player_score'];
+								$data['tg_player_rank'] = $rank;
+								$data['tg_ref_id'] = time().'e'.time().'c'.$userId;
+								$data['tg_player_reward'] = $array['prizes'][$rank];
+								$data['tg_is_gratify'] = 0;
+								$this->db->insert('tbl_talktime_gratification' , $data);
+								// die();
+							}
+							else
+							{
+								
+								$this->distributeTournamentReward($player['player_id'] , $rank , $array['prizes'][$rank]);
+								$this->updateRewardCoins($userId , $userInfo['user_reward_coins'] , $array['prizes'][$rank]);
+								$this->updateRewardCoinsHistory($userId, $array['prizes'][$rank]);
+								
+							}
+						} 
+						$rank++;
+					}
+					if($list[0]['fee_tournament_rewards']==3)
+					{
+						$result = $this->generateTokenForGratification();
+						if($result)
+						{
+							$allPlayer = $this->SITEDBAPI->getUserForGratification($t_id);
+							if(!empty($allPlayer))
+							{
+								foreach($allPlayer as $row)
+								{
+									print_r($row);
+								}
+							}
+						}
+					}
 
+				}
+				$updateResult['result_t_id'] = $t_id;
+				$updateResult['result_added_on'] = time();
+				$this->db->insert('tournaments_results', $updateResult);
+			} 
+		}
+	}
+}
+
+	function updateRewardCoins($userId , $existingCoins, $reward)
+	{
+		$data['user_reward_coins'] = $existingCoins + $reward;
+		$this->db->where('user_id', $userId);
+		$this->db->update('tbl_site_users' , $data);
+		if($this->db->affected_rows()>0)
+			return true;
+		return false;
+	}
+	function updateRewardCoinsHistory($userId , $reward)
+	{
+		$data['coin_user_id'] = $userId;
+		$data['coin_date'] = date('Y-m-d');
+		$data['coin_section'] = 5;
+		$data['coin_reward_coins_add'] = $reward;
+		$data['coin_type'] = 2;
+		$data['coin_added_on'] = time();
+		$this->db->insert('tbl_user_coins_history' , $data);
+	}
+	function distributeTournamentReward($userId, $rank , $reward)
+	{
+		$data['player_reward_rank'] = $rank;
+		$data['player_reward_prize'] = $reward;
+		$this->db->where('player_id' , $userId);
+		$this->db->update('tbl_tournaments_players' , $data);
+	}
+
+	function generateTokenForGratification()
+	{
+
+			$curl = curl_init();
+
+			curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://idp.ding.com/connect/token',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => 'client_secret=%2FTCCXOTVAXYfSv2%2FpuA5cFZiNOQjx%2F%2BkaYL9nZ6%2BKEY%3D&client_id=2337c2b8-4284-4e93-9d6e-344950c48422&grant_type=client_credentials',
+			CURLOPT_HTTPHEADER => array(
+				'Content-Type: application/x-www-form-urlencoded',
+				'Cookie: incap_ses_165_2345414=ukGgIR7Y7SiY2IMzpzJKAlPVDGIAAAAA54xRLasBz5wLT9c342lSSg==; nlbi_2345414=eNVPXLXiCU/cz4u5vZyyQgAAAADEhX9ZhB2UDMA+8bpNgzEn; visid_incap_2345414=Ah5F2bALQkSNDza4S5FYYVLVDGIAAAAAQUIPAAAAAACGKGTQPj3FD/02uO6/CeeK'
+			),
+			));
+
+			$response = curl_exec($curl);
+
+			curl_close($curl);
+			
+			$result = json_decode($response);
+			if(isset($result->access_token))
+			{
+				return $result;
+			}
+			else
+			{
+				return false;
+			}
+	}
+
+// END 
 }
